fa9c28d104430945fac31cbd5f79c0d1869d9746
Walt Ritscher <waltritscher@outlook.com>
2018-10-17 19:29:02 -0700

    Add RouxAcademy project

---
+++ RouxAcademy/Pages/StudentLoan.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RouxAcademy.Pages
+{
+  public class StudentLoanModel : PageModel
+  {
+    public string Message { get; set; }
+
+
+    [BindProperty]
+    public Models.Loan StudentLoan { get; set; }
+
+    public void OnGet()
+    {
+      StudentLoan =  new Models.Loan { Amount = 600, Rate = .12M, LengthInMonths = 24 };
+
+      Message = "Your contact page.";
+    }
+
+
+  }
+}
