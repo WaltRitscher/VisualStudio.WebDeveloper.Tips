3508da704d9edb157e32d444858336beb1d2a42e
Walt Ritscher <waltritscher@outlook.com>
2018-10-19 13:24:04 -0700

    Add more refactoring code

--- BasicCoreApp/Pages/CodeRefactorPage.cshtml.cs
+++ BasicCoreApp/Pages/CodeRefactorPage.cshtml.cs
@@ -19,23 +19,40 @@ namespace BasicCoreApp.Pages
     private void RefactorHere()
     {
       // Visual Studio includes some analyzers, refactorings
-      // Roslynator adds 500+  analyzers, refactorings
+      // Roslynator adds 500+ analyzers, refactorings
 
-     
+      // use string.empty
+      string s = "";
 
-      
+      // use string.IsNullOrEmpty
+      if (s == null || s.Length == 0)
+      {
+      }
+
     }
 
     private bool MoreRefactor()
     {
       // remove redundant parenthesis
-      if ((x)) 
+      if ((x))
       {
-        return (y); 
+        return (y);
       }
       return true;
     }
 
-   
+
+  }
+
+  public enum TourType
+  {
+    Bike = 4,
+    Walking = 2,
+    Cruise = 1,
+
+  }
+
+  public enum Empty {
+    None=0,
   }
 }
